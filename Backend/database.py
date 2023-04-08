f50cae98604d4397ca0ff4398b9e566062d5dd2a
Maciej <49777455+MaciekGorzny@users.noreply.github.com>
2023-04-08 17:07:18 +0200

    zaliczenie

---
+++ Backend/database.py
@@ -0,0 +1,22 @@
+import os
+import sqlalchemy as _sql
+import sqlalchemy.ext.declarative as _declarative
+import sqlalchemy.orm as _orm
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from dotenv import load_dotenv
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/postgres"
+
+engine = _sql.create_engine(DATABASE_URL)
+
+SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = _declarative.declarative_base()
