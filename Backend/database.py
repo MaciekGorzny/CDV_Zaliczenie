4db1fe76cd06c4b37af7777d50714d93453d9499
Maciej <49777455+MaciekGorzny@users.noreply.github.com>
2023-04-08 18:10:26 +0200

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,19 +1,19 @@
-import os
+#import os
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+#POSTGRES_USER = os.getenv("POSTGRES_USER")
+#POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+#POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/postgres"
+DATABASE_URL = "postgresql://postgres:postgres@10.0.1.4:5432/postgres"
 
 engine = _sql.create_engine(DATABASE_URL)
 
